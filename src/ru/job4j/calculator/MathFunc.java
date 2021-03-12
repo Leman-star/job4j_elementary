5a63e81f25ae2710790b8aa16a4240e2de40a1bb
Leman-star <viktor_6190@mail.ru>
2021-03-12 22:04:45 +0300

    5. Конвертер валюты [#489112]

---
+++ src/ru/job4j/calculator/MathFunc.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculator;
+
+public class MathFunc {
+    public static int func1(int x){
+        int y = x * x + 1;
+        return y;
+    }
+    public static int func2( int x){
+        int y = 1/x;
+        return y;
+    }
+    public static void main (String[] args){
+       int resultat1 = MathFunc.func1 (100);
+       int resultat2 = MathFunc.func2 (5);
+       int total = resultat1 + resultat2;
+       System.out.println(total);
+    }
+}
