4efe411fa98691f0817ff4405babbaf99be5c32d
Leman-star <viktor_6190@mail.ru>
2021-03-16 23:58:30 +0300

    9.1. String [#489123]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String name = "But I am a newbie.";
+        String separate = " ";
+        int year = 2021;
+        String id = idea + separate + name + separate + year + " year.";
+        System.out.println(id);
+
+    }
+}
