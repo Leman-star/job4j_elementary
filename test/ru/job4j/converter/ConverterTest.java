2f0bdb5da9ffac17766ba5cf2584f20f73c57fe0
Leman-star <viktor_6190@mail.ru>
2021-03-19 22:17:25 +0300

    1. Модульные тесты. [#489146]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        int rub = 180;
+        int dollar = 4;
+        int fro = Converter.rubleToDollar(rub);
+        Assert.assertEquals(dollar, fro);
+    }
+}
