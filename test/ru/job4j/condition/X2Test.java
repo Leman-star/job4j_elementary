1a9479295b2b03ecb12e8f3a6967151356f584cf
Leman-star <viktor_6190@mail.ru>
2021-03-21 21:22:05 +0300

    1.1. Входные данные в тесте [#489151]

--- test/ru/job4j/condition/X2Test.java
+++ test/ru/job4j/condition/X2Test.java
@@ -22,7 +22,7 @@ public class X2Test {
         Assert.assertEquals(expected, rsl);
     }
     @Test
-    public void whenA10B0C0X2Then3() {
+    public void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -32,7 +32,7 @@ public class X2Test {
         Assert.assertEquals(expected, rsl);
     }
     @Test
-    public void whenA10B0C0X2Then2() {
+    public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
@@ -42,7 +42,7 @@ public class X2Test {
         Assert.assertEquals(expected, rsl);
     }
     @Test
-    public void whenA10B0C0X2Then5() {
+    public void whenA1B1C0X1Then5() {
         int a = 1;
         int b = 1;
         int c = 0;
@@ -52,7 +52,7 @@ public class X2Test {
         Assert.assertEquals(expected, rsl);
     }
     @Test
-    public void whenA10B0C0X2Then1() {
+    public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
